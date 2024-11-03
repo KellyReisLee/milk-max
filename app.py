7d3eb8b3d9ba581e2a6a9cf5596d2dd12f0989a2
millennium164 <milleneyoshinorssuo@outlook.com>
2024-11-03 16:15:31 -0300

    basic navbar

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template, redirect, session
+from flask import Flask, render_template, redirect, session, request
+import psycopg2
+import os
 
 # Configure app
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY', 'default_secret_key')
+
+# Connect to the PostgreSQL database
+conn = psycopg2.connect(
+    dbname='milkmax',
+    user='postgres',
+    password='my627267',
+    host='localhost',
+    port='5432'
+)
+
+# Create a cursor object
+cursor = conn.cursor()
+
 
 @app.route("/")
 def index():
@@ -10,3 +26,5 @@ def index():
 
 if __name__=='__main__':
     app.run(debug=True)
+
+conn.close()
