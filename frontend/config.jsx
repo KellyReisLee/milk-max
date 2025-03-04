29f784830a4683c150584ac30ec5057e471e4dfe
Millene <milleneyoshinorusso@outlook.com>
2025-03-04 14:32:40 -0300

    config

--- frontend/config.jsx
+++ frontend/config.jsx
@@ -1,17 +1,18 @@
 export const config = {
-    backendUrl: process.env.REACT_APP_BACKEND_URL,
+    backendUrl: process.env.VITE_BACKEND_URL,
     routes: {
-      contato: `${process.env.REACT_APP_BACKEND_URL}/contato`,
-      login: `${process.env.REACT_APP_BACKEND_URL}/login`,
-      diario: `${process.env.REACT_APP_BACKEND_URL}/diario`,
-      forgot_pass: `${process.env.REACT_APP_BACKEND_URL}/forgot/password`,
-      forgot_user: `${process.env.REACT_APP_BACKEND_URL}/forgot/username`,
-      reset_pass: `${process.env.REACT_APP_BACKEND_URL}/reset_password`,
-      relatorios: `${process.env.REACT_APP_BACKEND_URL}/relatorios`,
-      signup: `${process.env.REACT_APP_BACKEND_URL}/signup`,
-      vacas: `${process.env.REACT_APP_BACKEND_URL}/vacas`,
-      cadastro: `${process.env.REACT_APP_BACKEND_URL}/cadastro`,
-      registro: `${process.env.REACT_APP_BACKEND_URL}/registro`,
+      contato: `${process.env.VITE_BACKEND_URL}/contato`,
+      login: `${process.env.VITE_BACKEND_URL}/login`,
+      diario: `${process.env.VITE_BACKEND_URL}/diario`,
+      forgot_pass: `${process.env.VITE_BACKEND_URL}/forgot/password`,
+      forgot_user: `${process.env.VITE_BACKEND_URL}/forgot/username`,
+      reset_pass: `${process.env.VITE_BACKEND_URL}/reset_password`,
+      relatorios: `${process.env.VITE_BACKEND_URL}/relatorios`,
+      signup: `${process.env.VITE_BACKEND_URL}/signup`,
+      vacas: `${process.env.VITE_BACKEND_URL}/vacas`,
+      cadastro: `${process.env.VITE_BACKEND_URL}/cadastro`,
+      registro: `${process.env.VITE_BACKEND_URL}/registro`,
       // Adicione outras rotas aqui
     },
   };
+
